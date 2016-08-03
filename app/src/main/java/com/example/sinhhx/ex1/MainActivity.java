08251cff5ffe8213be9f673be674ef49b45079fe
SinhHa <sinh.ha1992@gmail.com>
2016-08-03 12:14:37 +0700

    Init project

---
+++ app/src/main/java/com/example/sinhhx/ex1/MainActivity.java
@@ -0,0 +1,67 @@
+package com.example.sinhhx.ex1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import android.widget.Button;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    EditText email ;
+    EditText CheckPhone;
+    String checkString;
+    String phone;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final Button button = (Button) findViewById(R.id.btn1);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                email = (EditText)findViewById(R.id.Text3);
+                CheckPhone = (EditText)findViewById(R.id.Text4);
+              checkString = email.getText().toString().trim();
+                phone= CheckPhone.getText().toString().trim();
+                if ((checEmail(checkString)==false)) {
+                    Toast.makeText(getApplicationContext(),
+                            "Wrong Email Format", Toast.LENGTH_SHORT)
+                            .show();
+                }
+                if(checkPhone(phone)==false){
+                    Toast.makeText(getApplicationContext(),
+                            "Invalid phone number", Toast.LENGTH_SHORT)
+                            .show();
+                }
+
+                else{
+                    Toast.makeText(getApplicationContext(),
+                            "success", Toast.LENGTH_SHORT)
+                            .show();
+                }
+
+            }
+        });
+    }
+    public boolean checEmail(String mail){
+        String emailRegex ="^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+        if(mail.matches(emailRegex))
+        {
+            return true;
+        }
+        return false;
+    }
+    public boolean checkPhone(String phone){
+        String regix="^[0-9]";
+        if(phone.matches(regix)){
+            return true;
+        }
+        else return false;
+    }
+
+        }
+
+
+
