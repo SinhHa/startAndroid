e7e7807bb1a22fd4447d5515655f309df0fbd1a3
SinhHa <sinh.ha1992@gmail.com>
2016-08-05 09:58:31 +0700

    Init project

--- app/src/main/java/com/example/sinhhx/ex1/Activity2.java
+++ app/src/main/java/com/example/sinhhx/ex1/Activity2.java
@@ -1,20 +1,7 @@
 package com.example.sinhhx.ex1;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.widget.EditText;
 
 /**
  * Created by sinhhx on 8/5/16.
  */
-public class Activity2 extends AppCompatActivity {
-    EditText email ;
-    EditText CheckPhone;
-    String checkString;
-    String phone;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-    }
+public class Activity2 {
 }
